c51f67b0117653c67bbc8bb7080fced85daf5c81
Jared Sepulveda <Sepulvedajared20@gmail.com>
2023-01-11 10:04:44 -0700

    MVP

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func flash(_ sender: Any) {
+        self.view.backgroundColor = UIColor.black
+    }
+    
+    @IBAction func Reset(_ sender: Any) {
+        self.view.backgroundColor = UIColor.white
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
